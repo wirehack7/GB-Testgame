e3e5ecaf1ed1a669eb5953f54d860de5bf4cf27c
wirehack7 <wirehack7@users.noreply.github.com>
2023-08-09 16:49:45 +0200

    it has a splash screen now!

--- src/StateStart.c
+++ src/StateStart.c
@@ -3,19 +3,17 @@
 #include "ZGBMain.h"
 #include "Scroll.h"
 #include "SpriteManager.h"
-#include "Print.h"
 #include "Keys.h"
+#include "Print.h"
 
-IMPORT_MAP(hud);
+IMPORT_MAP(splash);
 IMPORT_TILES(font);
 
+
 void START() {
-    INIT_HUD(hud);
-    INIT_FONT(font, PRINT_WIN);
-
-    PRINT(6, 2, "TestGame");
-    PRINT(4, 5, "Press START!");
-
+    InitScroll(BANK(splash), &splash, 0, 0);
+    INIT_FONT(font, PRINT_BKG);
+    PRINT(4, 12, "Press START");
 	NR52_REG = 0x80;
 	NR51_REG = 0xFF;
 	NR50_REG = 0x77;
