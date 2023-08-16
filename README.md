3ea5fa0a471e2cc3efe36ed55199865d1469280c
wirehack7 <wirehack7@users.noreply.github.com>
2023-08-16 16:56:24 +0200

    Added Music

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Testgame
 
 Just a small test to learn coding in C für GB.
+
+It uses ZGB
+
+## Notes
+
+Music from https://github.com/selkkie/acattag (MIT LICENSE)
--- src/StateStart.c
+++ src/StateStart.c
@@ -5,15 +5,18 @@
 #include "SpriteManager.h"
 #include "Keys.h"
 #include "Print.h"
+#include "Music.h"
 
 IMPORT_MAP(splash);
 IMPORT_TILES(font);
+DECLARE_MUSIC(title);
 
 void performantdelay(uint8_t numloops) BANKED; // Just testing banked func here
 
 void START() {
     InitScroll(BANK(splash), &splash, 0, 0);
     INIT_FONT(font, PRINT_BKG);
+    PlayMusic(title, 1);
 	NR52_REG = 0x80;
 	NR51_REG = 0xFF;
 	NR50_REG = 0x77;
